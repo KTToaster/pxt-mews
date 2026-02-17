51db4f1c9263678226e1748951e73e1d24320d2b
KTToaster <K.D.Tomkins@outlook.com>
2026-02-17 10:09:00 +0000

    Initial Commit

---
+++ source/tannoy.cpp
@@ -0,0 +1,165 @@
+#include "pxt.h"
+#include "MicroBit.h"
+#include "mappings.h"
+#include "utils/decompressor.h"
+#include "audio/audio.h"
+#include <string>
+using namespace pxt;
+
+void init_timer(int sample_rate);
+void init_pwm();
+void play_wav(uint8_t* audio_data, size_t out_size);
+void play_sample(uint16_t sample);
+void announce_number(uint8_t number);
+void announce_number_special(uint8_t special);
+
+
+namespace tannoy{
+  //%
+  void announceStation(int station){
+    #if MICROBIT_CODAL
+    name_data_mapping station_obj = audio::MAPPINGS[station];
+    size_t out_size = 0;
+    uint8_t* audio_data = decompress(station_obj.data, station_obj.size, &out_size); // U8 PCM Wav Data
+
+    init_timer(station_obj.sample_rate);
+    init_pwm(); // Initialises PWM
+    uBit.display.scroll(station_obj.name);
+
+    play_wav(audio_data, out_size);
+    
+    free(audio_data);
+    uBit.display.scroll(station_obj.name);
+    #else
+    target_panic(PANIC_VARIANT_NOT_SUPPORTED);
+    #endif
+  }
+
+  //%
+  void announceTime(int hours, int mins){
+    #if MICROBIT_CODAL
+    char hours_str[3], mins_str[3];
+    itoa(hours, hours_str);
+    itoa(mins, mins_str);
+    char* _time_str = strcat(hours_str, ":");
+    char* time_str = strcat(_time_str, mins_str);
+    // Play Hours
+    uBit.display.scroll(ManagedString(time_str));
+    if(hours == 0){
+      announce_number(0);
+    }else if(hours < 10){
+      announce_number(0);
+      announce_number(hours);
+    }else if(hours < 20){
+      announce_number(hours);
+    }else{
+      announce_number(20);
+      if(hours-20 != 0){
+        announce_number(hours-20);
+      }
+    }
+    uBit.sleep(200);
+
+    // Play Mins
+    if(mins == 0){
+      announce_number_special(0);
+    }else if(mins < 10){
+      announce_number(0);
+      announce_number(hours);
+    }else if(mins < 20){
+      announce_number(hours);
+    }else{
+      for(int i=20; i<60; i+=10){
+        if(!mins-i<10){
+          continue;
+        }
+        announce_number(i);
+        if(mins-i != 0){
+          announce_number(mins-i);
+        }
+      }
+    }
+    #else
+    target_panic(PANIC_VARIANT_NOT_SUPPORTED);
+    #endif
+  }
+}
+
+#if MICROBIT_CODAL
+void announce_number(uint8_t number){
+  num_data_mapping num_obj = times::MAPPINGS[number];
+
+  init_timer(num_obj.sample_rate); 
+  init_pwm(); // Initialises PWM
+
+  size_t out_size = 0;
+  uint8_t* audio_data = decompress(num_obj.data, num_obj.size, &out_size); // U8 PCM Wav Data
+
+  
+  play_wav(audio_data, out_size);
+  free(audio_data);
+}
+
+void announce_number_special(uint8_t special){
+  name_data_mapping num_obj = times::MAPPINGS_SPECIAL[special];
+
+  init_timer(num_obj.sample_rate); 
+  init_pwm(); // Initialises PWM
+
+  size_t out_size = 0;
+  uint8_t* audio_data = decompress(num_obj.data, num_obj.size, &out_size); // U8 PCM Wav Data
+
+  play_wav(audio_data, out_size);
+  free(audio_data);
+}
+
+void play_wav(uint8_t* audio_data, size_t out_size){
+  for(unsigned int i=0; i<out_size/sizeof(uint8_t); i++){
+    NRF_TIMER0->TASKS_CLEAR=(TIMER_TASKS_CLEAR_TASKS_CLEAR_Trigger<<TIMER_TASKS_CLEAR_TASKS_CLEAR_Pos);                         // Resets the timer
+    NRF_TIMER0->EVENTS_COMPARE[0]=(TIMER_EVENTS_COMPARE_EVENTS_COMPARE_NotGenerated<<TIMER_EVENTS_COMPARE_EVENTS_COMPARE_Pos);  // Resets the comparison check
+
+    // Play Sample (Not Functioning Correctly)
+    play_sample((uint16_t)audio_data[i]*2);
+    
+    // Wait for timer to finish
+    while(NRF_TIMER0->EVENTS_COMPARE[0] == (TIMER_EVENTS_COMPARE_EVENTS_COMPARE_NotGenerated << TIMER_EVENTS_COMPARE_EVENTS_COMPARE_Pos));
+  }
+  NRF_TIMER0->TASKS_STOP = (TIMER_TASKS_STOP_TASKS_STOP_Trigger<<TIMER_TASKS_STOP_TASKS_STOP_Pos);                              // Stops the timer
+  NRF_PWM0->ENABLE = (PWM_ENABLE_ENABLE_Disabled << PWM_ENABLE_ENABLE_Pos);                                                     // Disables the timer    
+}
+
+
+void play_sample(uint16_t sample){
+  volatile uint16_t sequence[1] = {(sample * NRF_PWM0->COUNTERTOP) / 255};                                            // Defines a sequence
+
+  NRF_PWM0->SEQ[0].PTR = ((uint32_t)&sequence << PWM_SEQ_PTR_PTR_Pos);                                                // Finds the start of the sequence 
+  NRF_PWM0->SEQ[0].CNT = (1 << PWM_SEQ_CNT_CNT_Pos);                                                                  // Defines the size of the sequence as 1 
+  NRF_PWM0->TASKS_SEQSTART[0] = (PWM_TASKS_SEQSTART_TASKS_SEQSTART_Trigger << PWM_TASKS_SEQSTART_TASKS_SEQSTART_Pos); // Enables the Sequence
+}
+
+void init_timer(int sample_rate){
+  // Timer Initialisation
+  NRF_TIMER0->PRESCALER = (4UL << TIMER_PRESCALER_PRESCALER_Pos);                                         // Sets the prescaler to divide the clock by 4
+  NRF_TIMER0->BITMODE = (TIMER_BITMODE_BITMODE_32Bit << TIMER_BITMODE_BITMODE_Pos);                       // Sets the bitmode to 32bits
+
+  NRF_TIMER0->CC[0] = 1000000/sample_rate;                                                                // Sets how long to wait for before returning True
+  NRF_TIMER0->TASKS_START = (TIMER_TASKS_START_TASKS_START_Trigger << TIMER_TASKS_START_TASKS_START_Pos); // Starts the timer
+}
+
+void init_pwm(){
+  NRF_PWM0->PSEL.OUT[0] = (MICROBIT_PIN_SPEAKER << PWM_PSEL_OUT_PIN_Pos) |                // Maps the first pin to the speaker
+                          (PWM_PSEL_OUT_CONNECT_Connected << PWM_PSEL_OUT_CONNECT_Pos);   // Connects the pin
+      
+  NRF_PWM0->ENABLE = (PWM_ENABLE_ENABLE_Enabled << PWM_ENABLE_ENABLE_Pos);                // Enables PWM
+
+  NRF_PWM0->DECODER = (PWM_DECODER_LOAD_Common << PWM_DECODER_LOAD_Pos);                  // Specifies the Common Decoder
+  NRF_PWM0->MODE = (PWM_MODE_UPDOWN_Up << PWM_MODE_UPDOWN_Pos);                           // Specifies the mode to be up-only
+  
+  NRF_PWM0->PRESCALER = (PWM_PRESCALER_PRESCALER_DIV_1 << PWM_PRESCALER_PRESCALER_Pos);   // Specifies the prescaler to divide the clock by 0
+  NRF_PWM0->COUNTERTOP = 1020;
+
+  NRF_PWM0->SEQ[0].REFRESH  = (0 << PWM_SEQ_REFRESH_CNT_Pos);                             // Disables Refreshing
+  NRF_PWM0->SEQ[0].ENDDELAY = (0 << PWM_SEQ_ENDDELAY_CNT_Pos);                            // Disables End Delay
+  NRF_PWM0->LOOP = (PWM_LOOP_CNT_Disabled << PWM_LOOP_CNT_Pos);                           // Disables Looping
+}
+#endif
---
+++ source/utils/decompressor.cpp
@@ -0,0 +1,49 @@
+#include "pxt.h"
+#include "decompressor.h"
+#include "../mappings.h"
+
+// FUNCTION DEFINITIONS //
+uint8_t* decompress(const uint8_t* data, size_t data_len, size_t* out_len){
+  int predictor = 0;
+  int index = 0;      
+  int step = step_table[index];
+
+  size_t pcm_len = data_len * 2;
+  uint8_t* pcm = (uint8_t*)malloc(pcm_len);
+
+  size_t pcm_pos = 0;
+
+  for (int i = 0; i < data_len; i++) {
+    uint8_t byte = data[i];
+
+    for(int nibble = 0;nibble < 2;nibble++){
+      uint8_t code = (nibble == 0) ? (byte & 0x0F) : (byte >> 4);
+
+      int diff = step >> 3;
+      if (code & 1) diff += step >> 2;
+      if (code & 2) diff += step >> 1;
+      if (code & 4) diff += step;
+      if (code & 8) diff = -diff;
+
+      predictor += diff;
+
+      // Clamp to 16-bit range
+      if (predictor > 32767) predictor = 32767;
+      if (predictor < -32768) predictor = -32768;
+
+      // Update index
+      index += index_table[code];
+      if (index < 0) index = 0;
+      if (index > 88) index = 88;
+
+      step = step_table[index];
+
+      // Convert to u8 PCM 0–255
+      int pcm_val = (predictor + 32768) >> 8; 
+      pcm[pcm_pos++] = (uint8_t)pcm_val;
+    }
+  }
+
+  *out_len = pcm_len;
+  return pcm;
+}
