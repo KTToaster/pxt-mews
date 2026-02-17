a1e9e44977a1c6bf2cfbcb4b7563faf7762ac292
KTToaster <K.D.Tomkins@outlook.com>
2026-02-17 10:12:11 +0000

    Hotfix: Update cpp dependencies

--- source/audio/audio.h
+++ source/audio/audio.h
@@ -9,21 +9,21 @@
 #include "numbers_minimum.h"
 #include "stations_minimum.h"
 
-#elif AUDIO_QUALITY == 1
-#include "numbers_low.h"
-#include "stations_low.h"
+// #elif AUDIO_QUALITY == 1
+// #include "numbers_low.h"
+// #include "stations_low.h"
 
-#elif AUDIO_QUALITY == 2
-#include "numbers_medium.h"
-#include "stations_medium.h"
+// #elif AUDIO_QUALITY == 2
+// #include "numbers_medium.h"
+// #include "stations_medium.h"
 
-#elif AUDIO_QUALITY == 3
-#include "numbers_high.h"
-#include "stations_high.h"
+// #elif AUDIO_QUALITY == 3
+// #include "numbers_high.h"
+// #include "stations_high.h"
 
-#else
-#include "numbers_extreme.h"
-#include "stations_extreme.h"
+// #else
+// #include "numbers_extreme.h"
+// #include "stations_extreme.h"
 
 #endif
 #endif
