c2a0a6ccf2fad0dd9cb6dee9812fe9d2fe1cb9b4
KTToaster <K.D.Tomkins@outlook.com>
2026-02-20 15:17:15 +0000

    update variable compression code

--- source/audio/audio.h
+++ source/audio/audio.h
@@ -1,29 +1,52 @@
 #ifndef __AUDIO_H
 #define __AUDIO_H
 
-#ifndef AUDIO_QUALITY
-#define AUDIO_QUALITY 0
-#endif
+namespace minimum_audio{
+  #include "numbers_minimum.h"
+  #include "stations_minimum.h"
+}
 
-#if AUDIO_QUALITY == 0
-#include "numbers_minimum.h"
-#include "stations_minimum.h"
+namespace low_audio{
+  #include "numbers_low.h"
+  #include "stations_low.h"
+}
+namespace medium_audio{
+  #include "numbers_medium.h"
+  #include "stations_medium.h"
+}
+namespace high_audio{
+  #include "numbers_high.h"
+  #include "stations_high.h"
+}
+namespace extreme_audio{
+  #include "numbers_extreme.h"
+  #include "stations_extreme.h"
+}
 
-#elif AUDIO_QUALITY == 1
-#include "numbers_low.h"
-#include "stations_low.h"
+static uint8_t quality=0;
 
-#elif AUDIO_QUALITY == 2
-#include "numbers_medium.h"
-#include "stations_medium.h"
+// Audio Quality Shims
+// Must be literal or the compiler will load every audio file
+//%
+void setAudioQuality_min(){ 
+  quality=1;
+}
+//%
+void setAudioQuality_low(){ 
+  quality=2;
+}
+//%
+void setAudioQuality_med(){ 
+  quality=3;
+}
+//%
+void setAudioQuality_high(){ 
+  quality=4;
+}
+//%
+void setAudioQuality_extreme(){ 
+  quality=5;
+}
 
-#elif AUDIO_QUALITY == 3
-#include "numbers_high.h"
-#include "stations_high.h"
 
-#else
-#include "numbers_extreme.h"
-#include "stations_extreme.h"
-
-#endif
 #endif
