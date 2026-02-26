fbd9d80954a0f3a730822fde8fa6126371e2b1c1
KTToaster <K.D.Tomkins@outlook.com>
2026-02-26 15:38:35 +0000

    Update Pipeline for General TTS

--- source/audio.h
+++ source/audio.h
@@ -20,6 +20,16 @@ struct Audio_Data{
 // Data
 namespace audio{
   static std::map<const char*, Audio_Data> data_map;
+  
+  //%
+  void add_audio(const char* name, uint8_t* data, int size, int sample_rate) {
+    Audio_Data audioData;
+    audioData.data = data;
+    audioData.size = size;
+    audioData.sample_rate = sample_rate;
+
+    data_map[name] = audioData;
+  }
 }
 
 #endif
