b0b6d6918a2bb973324f266de47a644d3467e713
Robert Knight <robert@microbit.org>
2026-02-27 16:59:42 +0000

    payload -> data

--- source/audio.cpp
+++ source/audio.cpp
@@ -6,7 +6,7 @@ namespace audio{
   void add_audio(String name, Buffer data, int data_len, int sample_rate){
     uBit.display.scroll(ManagedString(name->getUTF8Data())); // temporary debug
     Audio_Data audioData;
-    audioData.data = data->payload;
+    audioData.data = data->data;
     audioData.size = data_len;
     audioData.sample_rate = sample_rate;
     data_map[std::string(name->getUTF8Data())] = audioData;
