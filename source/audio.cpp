bf884883d635ecb9d9d36d02c37b47cbe87c3405
KTToaster <K.D.Tomkins@outlook.com>
2026-02-27 09:51:55 +0000

    Fix Audio Length Value

--- source/audio.cpp
+++ source/audio.cpp
@@ -3,10 +3,10 @@
 
 namespace audio{
   //%
-  void add_audio(const char* name, uint8_t* data, int sample_rate){
+  void add_audio(const char* name, uint8_t* data, int data_len, int sample_rate){
     Audio_Data audioData;
     audioData.data = data;
-    audioData.size = sizeof(data);
+    audioData.size = data_len;
     audioData.sample_rate = sample_rate;
 
     data_map[name] = audioData;
