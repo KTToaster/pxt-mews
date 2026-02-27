e94b7d7af6188fb33cf373e5f43ffa53afa96452
Robert Knight <robert@microbit.org>
2026-02-27 16:49:30 +0000

    Debug

--- source/audio.cpp
+++ source/audio.cpp
@@ -4,6 +4,7 @@
 namespace audio{
   //%
   void add_audio(const char* name, uint8_t* data, int data_len, int sample_rate){
+    uBit.display.scroll(ManagedString(name)); // temporary debug
     Audio_Data audioData;
     audioData.data = data;
     audioData.size = data_len;
